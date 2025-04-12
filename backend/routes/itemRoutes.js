56dcfcc2b2cca00338b0c1343dd39421d52642e7
Alok <advancedtechnology62@gmail.com>
2025-04-12 13:06:31 +0530

    done

--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const { createItem, getAllItems, updateItem, deleteItem } = require('../controllers/itemController');
+const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 // Create Item
-router.post('/create', createItem);
+router.post('/create', protect, createItem);
 
 // Get All Items
 router.get('/', getAllItems);
 
 // Update Item by ID
-router.put('/:id', updateItem);
+router.put('/:id', protect, updateItem);
 
 // Delete Item by ID
-router.delete('/:id', deleteItem);
+router.delete('/:id', protect, deleteItem);
 
 module.exports = router;
